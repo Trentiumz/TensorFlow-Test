19c493f7fc2788be907b9f8936148307ac34b88b
Daniel Ye <danielyeqt@gmail.com>
2021-03-11 20:12:59 -0500

    Increase Epoch

--- Uno/1-basic_line_fitting.py
+++ Uno/1-basic_line_fitting.py
@@ -1,12 +1,13 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-learning_rate = 0.001
-epochs = 10
+w_learning_rate = 0.0001
+epochs = 2000
 my_batch_size = 50
+b_learning_rate = w_learning_rate * my_batch_size
 
 my_feature = range(my_batch_size)
-my_label = np.array([-3 * x for x in range(my_batch_size)]) + (np.random.random(my_batch_size) * 100 - 50)
+my_label = np.array([-3 * x for x in range(my_batch_size)]) + (np.random.random(my_batch_size) * 100 - 50) + 50
 
 
 # [w, b]
@@ -17,7 +18,7 @@ def build_model():
 def train_model(model):
     errors = []
     for i in range(epochs):
-        plot_model(model)
+        # plot_model(model)
         total_w_grad = 0
         total_b_grad = 0
         total_loss = 0
@@ -26,12 +27,14 @@ def train_model(model):
             y = my_label[index]
 
             total_loss += (y - model[1] - model[0] * x)**2
+
             total_w_grad += 2 * (y - model[1] - model[0] * x) * x
             total_b_grad += 2 * (y - model[1] - model[0] * x)
-        model[0] += total_w_grad / my_batch_size * learning_rate
-        model[1] += total_b_grad / my_batch_size * learning_rate
-        print(f"{total_w_grad} {total_b_grad} {total_w_grad / my_batch_size} {total_b_grad / my_batch_size}")
+        model[0] += total_w_grad / my_batch_size * w_learning_rate
+        model[1] += total_b_grad / my_batch_size * b_learning_rate
+        # print(f"{total_loss} {total_w_grad} {total_b_grad} {total_w_grad / my_batch_size} {total_b_grad / my_batch_size}")
         errors.append(total_loss)
+        # plot_model(model)
     return range(epochs), errors
 
 
@@ -39,6 +42,7 @@ def plot_model(model):
     plt.xlabel("feature")
     plt.ylabel("label")
 
+    print(model)
     plt.scatter(my_feature, my_label)
     x0 = 0
     xf = max(my_feature)
